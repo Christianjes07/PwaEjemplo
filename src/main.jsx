ad5e86abdff0f82b7a02a67f00f76c41ecad74bd
Christianjes07 <100322673+Christianjes07@users.noreply.github.com>
2024-05-24 11:09:36 -0600

    Create index.html

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App.jsx'
+import './index.css'
+import * as serviceWorkerRegistration from '../public/serviceWorkerRegistration.js';
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+serviceWorkerRegistration.register();
+
